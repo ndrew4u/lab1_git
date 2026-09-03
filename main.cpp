9efc826c1c1955cb635bb866d68ba25d7f149aea
ndrew4u <aivanov.2005@mail.ru>
2026-09-03 20:52:00 +0700

    Добавлена вторая расчтеная функция и пункт меню

--- main.cpp
+++ main.cpp
@@ -3,20 +3,27 @@
 #include <iostream>
 #include<locale.h>
 using namespace std;
+const double g = 9.81;
 // ===== Расчётные функции варианта =====
 // Сила тяжести тела массой m
-double gravityForce(double m) {
-    const double g = 9.81;
+double gravityForce(double m) 
+{
     return m * g;
+}
+double weightInLift(double m, double a)
+{
+    return m * (g + a);
 }
 // ===== Главная функция: меню =====
 int main() {
     setlocale(LC_ALL, "RUS");
     int choice;
     double m;
+    double a;
     do {
         cout << "\n*** Вариант 32: Сила тяжести***\n";
         cout << "1. Сила тяжести\n";
+        cout << "2. Сила тяжести в лифте\n";
         cout << "0. Выход\n";
         cout << "Выберите пункт: ";
         cin >> choice;
@@ -30,6 +37,18 @@ int main() {
         } while (m <= 0);
             cout << "Сила тяжести = " << gravityForce(m) << "\n";
             break;
+        case 2:
+        {
+            do
+            {
+                cout << "Введите массу m: ";
+                cin >> m;
+                if (m <= 0) { cout << "Попробуйте снова!\n"; }
+            } while (m <= 0);
+            cout << "Введите ускорение лифта: ";
+            cin >> a;
+            cout << "Сила тяжести = " << weightInLift(m, a) << "\n";
+        }
         case 0:
             cout << "Работа завершена.\n";
             break;
